167d1973107d0e9e25087099953786475aa2c92d
ajeet goswmi <ajeet.2015.gos@gmail.com>
2026-07-07 17:41:13 +0530

    working with puppeteer

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -157,13 +157,10 @@ const targetResumeSchema = {
 };
 
 async function utilityResumePdf(htmlContent) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
+  const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
-  await page.setContent(htmlContent, { waitUntil: "networkidle2" });
+  await page.setContent(htmlContent, { waitUntil: "networkidle0" });
 
   const pdfBuffer = await page.pdf({
     format: "A4",
